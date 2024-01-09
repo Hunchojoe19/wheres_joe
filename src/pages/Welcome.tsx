6d42f5d32c4dc41d55cd5d6fd5fb670251028807
Joseph Enubiak <enubiakjoseph@gmail.com>
2024-01-09 01:54:09 +0100

    ..

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -22,6 +22,31 @@ const welcomeVariants = {
       stiffness: 120
     }
     
+  }, 
+  exit: {
+    opacity: 0,
+    y: "1000vh"
+  }
+}
+const imgVariants = {
+  hidden: {
+    opacity: 0,
+    x: "-100vh"
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+      delay: 0.5,
+      type: "spring",
+      stiffness: 120
+    }
+    
+  }, 
+  exit: {
+    opacity: 0,
+    x: "1000vh"
   }
 }
 
@@ -37,7 +62,7 @@ export const Welcome = () => {
   }, [account, navigate]);
 
   return (
-    <motion.div className='flex h-full w-full items-stretch justify-between'>
+    <div className='flex h-full w-full items-stretch justify-between'>
       <div className='relative flex h-full w-full flex-col items-center justify-center'>
         {/* <img
           src={rightImageSrc}
@@ -49,15 +74,24 @@ export const Welcome = () => {
           alt='Left Alex'
           className='fixed left-0 top-1/4 h-full max-h-[20rem] max-w-[50%] -translate-y-20 object-contain'
         /> */}
-        <h1 className='text-24xl z-10 max-w-full overflow-visible whitespace-nowrap text-center font-extrabold leading-[40.56px] tracking-tight text-primary-white animate-bounce'>
+        <motion.h1 className='text-24xl z-10 max-w-full overflow-visible whitespace-nowrap text-center font-extrabold leading-[40.56px] tracking-tight text-primary-white animate-bounce'
+        
+        variants={welcomeVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit">
           WHERE'S
           <br />
           JOE?
-        </h1>
-        <p className='z-10 mb-8 mt-8 max-w-[400px] text-center text-base font-bold tracking-tight text-primary-white'>
+        </motion.h1>
+        <motion.p className='z-10 mb-8 mt-8 max-w-[400px] text-center text-base font-bold tracking-tight text-primary-white'
+        variants={welcomeVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit">
           A thrilling game showcasing the power of Aleo and the Puzzle
           multiparty privacy stack through a wager between friends!
-        </p>
+        </motion.p>
         <Button
           className='max-w-[250px]'
           onClick={connect}
@@ -66,12 +100,16 @@ export const Welcome = () => {
         >
           {loading ? 'Loading...' : loading ? 'Connecting...' : 'Play!'}
         </Button>
-        <img
-          src={bottomImageSrc}
+        <motion.img
+        variants={imgVariants}
+        initial="hidden"
+        animate="visible"  
+        exit="exit"        
+        src={bottomImageSrc}
           alt='Bottom Alex'
-          className='center -translate-y-100 fixed bottom-0 h-full max-h-[12rem] w-3/5 max-w-[35%] transform object-contain'
+          className='center -translate-y-100 fixed bottom-0 h-full max-h-[12rem] w-3 max-w-[35%] transform object-contain'
         />
       </div>
-    </motion.div>
+    </div>
   );
 };
